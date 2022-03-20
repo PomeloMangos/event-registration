ed7c3bdb441250a721cdf955237410f47a58cc3a
Yuko Zheng <hi@yuko.me>
2022-03-20 15:53:14 +0800

    Fixes img

--- test/Pomelo.Wow.EventRegistration.WCL.Tests/FetcherTests.cs
+++ test/Pomelo.Wow.EventRegistration.WCL.Tests/FetcherTests.cs
@@ -9,9 +9,12 @@ namespace Pomelo.Wow.EventRegistration.WCL.Tests
         [Fact]
         public async Task FetchCharacterTest()
         {
+            Fetcher.SetApiKey("577ca89dd52bb404003e3f50bab35bcf");
             var ch = await Fetcher.FetchAsync("√»–°Ë÷", "¡˙÷Æ’ŸªΩ", Models.CharactorRole.Tank, 3);
+            var ch2 = await Fetcher.FetchAsync("√»–°Ë÷", "¡˙÷Æ’ŸªΩ", Models.CharactorRole.Tank, 4);
 
             Assert.NotNull(ch);
+            Assert.NotNull(ch2);
         }
     }
 }
